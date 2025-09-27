291f511470a68400dd38d5650246d30c025b3566
transwiththeplans <transwiththeplans@gmail.com>
2025-09-27 15:31:05 -0400

    new file:   data/text/all_learnables.json 	new file:   include/battle_script_commands.c 	modified:   include/config/caps.h 	modified:   include/constants/opponents.h 	modified:   src/caps.c 	modified:   src/data/trainers.h 	modified:   src/data/trainers.party

--- src/caps.c
+++ src/caps.c
@@ -10,14 +10,14 @@ u32 GetCurrentLevelCap(void)
     static const u32 sLevelCapFlagMap[][2] =
     {
         {FLAG_BADGE01_GET, 15},
-        {FLAG_BADGE02_GET, 19},
-        {FLAG_BADGE03_GET, 24},
-        {FLAG_BADGE04_GET, 29},
-        {FLAG_BADGE05_GET, 31},
-        {FLAG_BADGE06_GET, 33},
-        {FLAG_BADGE07_GET, 42},
-        {FLAG_BADGE08_GET, 46},
-        {FLAG_IS_CHAMPION, 58},
+        {FLAG_BADGE02_GET, 22},
+        {FLAG_BADGE03_GET, 30},
+        {FLAG_BADGE04_GET, 38},
+        {FLAG_BADGE05_GET, 40},
+        {FLAG_BADGE06_GET, 47},
+        {FLAG_BADGE07_GET, 51},
+        {FLAG_BADGE08_GET, 56},
+        {FLAG_IS_CHAMPION, 65},
     };
 
     u32 i;

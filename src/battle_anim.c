20fd8225e46e6388151a16779589f8ae82417881
izrofid <izrofid@gmail.com>
2025-09-29 05:11:11 +0000

    feat: add anim for web down

--- src/battle_anim.c
+++ src/battle_anim.c
@@ -253,6 +253,7 @@ static const u8* const sBattleAnims_General[NUM_B_ANIMS_GENERAL] =
     [B_ANIM_TERA_ACTIVATE]          = gBattleAnimGeneral_TeraActivate,
     [B_ANIM_SIMPLE_HEAL]            = gBattleAnimGeneral_SimpleHeal,
     [B_ANIM_POWER_CONSTRUCT]        = gBattleAnimGeneral_PowerConstruct,
+    [B_ANIM_STICKY_WEB]             = gBattleAnimGeneral_StickyWeb,
 };
 
 static const u8* const sBattleAnims_Special[NUM_B_ANIMS_SPECIAL] =

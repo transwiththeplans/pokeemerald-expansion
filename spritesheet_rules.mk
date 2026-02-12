6f73a1d262d886063de35b53b722c8ef232a369d
LuckyCapybara <215329213+LuckyCapybara@users.noreply.github.com>
2026-02-12 14:57:21 -0600

    Fix compilation rule for Youngster2 graphics

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -405,8 +405,8 @@ $(OBJEVENTGFXDIR)/people/woman_5.4bpp: %.4bpp: %.png
 
 $(OBJEVENTGFXDIR)/people/youngster.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
-	
-$(OBJEVENTGFXDIR)/people/youngster2.4bpp: %.4bpp: %.png
+
+$(OBJEVENTGFXDIR)/people/youngster_2.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
 

f919b04d65acbb34f8574c01a7f26ca0aaef8e60
BelialClover <67656448+BelialClover@users.noreply.github.com>
2026-02-11 19:53:04 -0600

    Event at Rustboro before first badge that introduces Innates

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -509,8 +509,7 @@
 #define FLAG_ENABLE_WINONA_MATCH_CALL        0x1D7
 #define FLAG_ENABLE_TATE_AND_LIZA_MATCH_CALL 0x1D8
 #define FLAG_ENABLE_JUAN_MATCH_CALL          0x1D9
-
-#define FLAG_UNUSED_0x1DA                    0x1DA // Unused Flag
+#define FLAG_LEARNED_ABOUT_INNATES           0x1DA
 
 #define FLAG_SHOWN_MYSTIC_TICKET             0x1DB
 #define FLAG_DEFEATED_HO_OH                  0x1DC

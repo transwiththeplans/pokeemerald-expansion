13bd8576a7030dac498d2de58efbb5d19b88c375
LuckyCapybara <215329213+LuckyCapybara@users.noreply.github.com>
2026-03-17 18:29:59 -0600

    Add battle against Kevin in Petalburg Fields

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -774,9 +774,7 @@
 #define FLAG_HIDE_PETALBURG_CITY_WALLY                              0x2D6
 #define FLAG_HIDE_MOSSDEEP_CITY_STEVENS_HOUSE_INVISIBLE_NINJA_BOY   0x2D7
 #define FLAG_HIDE_PETALBURG_CITY_WALLYS_MOM                         0x2D8
-
-#define FLAG_UNUSED_0x2D9                                           0x2D9 // Unused Flag
-
+#define FLAG_HIDE_PETALBURG_FIELDS_KEVIN                            0x2D9
 #define FLAG_HIDE_LILYCOVE_FAN_CLUB_INTERVIEWER                     0x2DA
 #define FLAG_HIDE_RUSTBORO_CITY_AQUA_GRUNT                          0x2DB
 #define FLAG_HIDE_RUSTBORO_CITY_DEVON_EMPLOYEE_1                    0x2DC
